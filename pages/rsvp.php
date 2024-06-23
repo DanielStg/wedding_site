a4c360e7e129c940a5944c46b64fbb97c6f7c3b2
Daniel St George <dan.stg@hotmail.com>
2024-06-23 17:39:06 +1200

    RSVP form submit. Alert confirmation. Form resets. Page redirect. JS moved to js file

--- pages/rsvp.php
+++ pages/rsvp.php
@@ -76,66 +76,9 @@
 -->
          </form>
 
-         <script>
-            //Create a const and select the form
-            const EL_formRSVP = document.querySelector("#form_rsvp");
-            
-            //Add an event listener for when the form submits
-            // addEventListener("type", func());
-            // (ev) => {} is creating function
-            EL_formRSVP.addEventListener("submit", (ev) => { 
-
-                //Prevents default form submit
-                ev.preventDefault();
-
-                //Fetch the data from the form action (submit)
-                fetch(EL_formRSVP.action, {
-                    method: 'POST', //POST method
-                    body: new FormData(EL_formRSVP), //Turns form data into JSON string
-                }).then(res => res.json()).then(data =>{
-                    console.log(data);
-                });
-                
-                
-            });
-                //.then(res => res.json()).then(data => { //res.json takes result json and creates js object from it
-                    // Hide the form                        Takes that object
-                    //EL_formRSVP.hidden = true;
-                    // Show the user name
-                    //document.querySelector("[data-fname]").textContent = data.fname;
-                //});
-            //});
-        </script> 
-                <!-- EXPLORE THIS JQUERY LATER - Might be simpler of above -->
-                <!-- $(document).ready(function(){
-                var $form = $('form');
-                $form.submit(function(){
-                    $.post($(this).attr('action'), $(this).serialize(), function(response){
-                            // do something here on success
-                    },'json');
-                    return false;
-                });
-                });
-                
-                // this is the id of the form
-                $("#idForm").submit(function(e) {
-
-                    e.preventDefault(); // avoid to execute the actual submit of the form.
-
-                    var form = $(this);
-                    var actionUrl = form.attr('action');
-                    
-                    $.ajax({
-                        type: "POST",
-                        url: actionUrl,
-                        data: form.serialize(), // serializes the form's elements.
-                        success: function(data)
-                        {
-                        alert(data); // show response from the php script.
-                        }
-                    });
-                    
-                });-->
+         <script src="../js/rsvp_functions.js"></script>
+        
+    
     </div>
         
  </body>
