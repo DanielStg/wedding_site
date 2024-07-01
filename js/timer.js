67c86123b527c279d7d261ec42aa4690f6362d3d
DanielStg <dan.stg@hotmail.com>
2024-07-01 12:09:25 +1200

    php and html tidy up

--- js/timer.js
+++ js/timer.js
@@ -1,12 +1,8 @@
-
 // Set the date we're counting down to
 var countDownDate = new Date("Feb 8, 2025 00:00:00").getTime();
 
-
-
 // Update the count down every 1 second
-var x = setInterval(function() {
-
+var x = setInterval(function () {
   // Get today's date and time
   var now = new Date().getTime();
 
@@ -18,11 +14,10 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
 
   // Display the result in the element with id="timer"
-  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("timer").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
