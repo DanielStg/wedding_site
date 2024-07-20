c014e4e97762d887c4a11ffa713d535f00c0c7ff
DanielStg <dan.stg@hotmail.com>
2024-07-21 10:07:45 +1200

    firebase implemented

--- js/rsvp_functions.js
+++ js/rsvp_functions.js
@@ -1,3 +1,32 @@
+import {
+  getDatabase,
+  ref,
+  child,
+  push,
+  update,
+  set,
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+
+const firebaseConfig = {
+  // ...
+  // The value of `databaseURL` depends on the location of the database
+  databaseURL: "https://wedding-32aed-default-rtdb.firebaseio.com/",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+
+function writeUserData(name, diet) {
+  const db = getDatabase();
+  set(ref(db, "GuestList/" + name), {
+    Diet: diet,
+  });
+}
+
 const EL_formRSVP = document.querySelector("#form_rsvp");
 
 //Add an event listener for when the form submits
@@ -10,10 +39,13 @@ EL_formRSVP.addEventListener("submit", (ev) => {
     method: "POST", //POST method
     body: new FormData(EL_formRSVP),
   })
-    .then((res) => res.text())
+    .then((res) => res.json())
     .then((data) => {
       //Gets the res and makes it a data object which displays json
-      console.log(data);
+      console.log(data[0].name);
+      console.log(data[0].diet);
+
+      writeUserData(data[0].name, data[0].diet);
       EL_formRSVP.reset();
       alert("Thankyou! You have successfully RSVP!");
       //document.location.href = "../index.html";
--- php/rsvp_scripts.php
+++ php/rsvp_scripts.php
@@ -10,8 +10,6 @@ if (
      $guestname = $_POST["fname"];
      $guestdiet = implode(',', $_POST["diet"]); //send array as string
 
-     $path ='../guests.json';
-
      $newData = [
         [
             "name" => $guestname,
@@ -19,40 +17,9 @@ if (
         ]
      ];
 
-    $inp = file_get_contents($path);
-    $temparray = json_decode($inp);
-    array_push($temparray, $newData);
-    $jsonData = json_encode($temparray, JSON_PRETTY_PRINT);
-    file_put_contents($path, $jsonData);
-
-//     $servername = "localhost";
-//     $username = "root";
-//     $password = "";
-//     $dbname = "wedding2024";
-
-//     // Create connection
-//     $conn = new mysqli($servername, $username, $password, $dbname);
-//     // Check connection
-//     if ($conn->connect_error) {
-//         echo "RSVP_SCRIPT: Server connection failed\n";
-//         die("Connection failed: " . $conn->connect_error);
-//     }
-//     $sql = "INSERT INTO guests(Name, Diet) VALUES ('$guestname', '$guestdiet')";
-
-//     if ($conn->query($sql) === TRUE) {
-        
-//         echo "RSVP_SCRIPT New record created successfully\n";
-//     } else {
-        
-//         echo "RSVP_SCRIPT Error: " . $sql . "<br>" . $conn->error;
-//     }
-    
-//     $conn->close();
-    
-     echo "RSVP_SCRIPT End\n";
+     echo json_encode($newData);
 }
-
-    
+  
 ?>
 
 
